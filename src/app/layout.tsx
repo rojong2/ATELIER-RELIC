9ccb1ab4f781028be132b18564407e0e63bb1543
rojong2 <ghwhd233@naver.com>
2026-03-26 18:36:11 +0900

    feat: vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
 import { Caudex } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 
 const caudex = Caudex({
   subsets: ["latin"],
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${caudex.className} min-h-screen bg-gray-500 text-white`}>
+        <Analytics />
         <div className="flex min-h-screen flex-col">
           <Header />
           <main className="flex-1 w-full">{children}</main>
